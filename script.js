575d4eae84a28a194d229cc8f929c8606f8522ee
Gokulcasper <ronitcasper215@gmail.com>
2022-06-14 22:11:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+// setInterval(function () {
+//   document.title = "GokulCasper";
+// }, 5000);
+
+// SCREEN OBJECT:
+//-----------------
+
+console.log(screen);
+console.log(
+  screen.width,
+  screen.height,
+  screen.availHeight,
+  screen.availWidth,
+  screen.colorDepth,
+  screen.pixelDepth
+);
+
+//  LOCATION OBJECT:
+//-------------------
+
+// console.log(location);
+// console.log(
+//   location.host,
+//   location.hostname,
+//   location.href,
+//   location.pathname,
+//   location.protocol
+// );
+// setInterval(() => {
+//   location.href = "https://google.com"; // href is a method
+// }, 5000);
+// if (location.protocol === "http:") {
+//   console.log("Unsecured HTTP");
+// } else {
+//   console.log("Secured HTTP");
+// }
+// setInterval(() => {
+//   location.assign("https://facebook.com");  // assign is a function
+// }, 5000);
+// console.log(document);
+
+// HISTORY OBJECT:
+//------------------
+
+// console.log(history);
+// history.back;
+// history.forward;
+// history.go;
+
+// NAVIGATOR OBJECT:
+//--------------------
+
+// console.log(navigator);
+// console.log(navigator.appName, navigator.appCodeName, navigator.platform);
